e15ebe4ab3c364f62cf2e729a3e6e33e47d8b79d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-11 12:32:42 +0300

    add executors

---
+++ src/com/jobready/threadpools/ThreadPoolDemo.java
@@ -0,0 +1,10 @@
+package com.jobready.threadpools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolDemo {
+
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+
+}
